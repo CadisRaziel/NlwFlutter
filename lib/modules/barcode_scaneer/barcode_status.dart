d888af16bbead0846695c93e7ecb1dc34e159519
CadisRaziel <vitorcsharp@hotmail.com>
2021-06-27 17:19:39 -0300

    resolvendo o problema do erro na camera

--- lib/modules/barcode_scaneer/barcode_status.dart
+++ lib/modules/barcode_scaneer/barcode_status.dart
@@ -1,47 +1,47 @@
+import 'package:camera/camera.dart';
+
 class BarcodeScannerStatus {
-  ///gerenciando o estado da camera !
-
-  ///isCameraAvailable = a camera esta pronta ?
-  final bool isCameraAvailable;
-
-  ///error = se tiver algum erro vamos armazenar ele
+final bool isCameraAvailable;
   final String error;
-
-  ///barcode = se tiver o barcode a gente vai armazenar ele
   final String barcode;
-
-  ///variavel para dar um stop no scanner
   final bool stopScanner;
 
+  BarcodeScannerStatus({
+    this.stopScanner = false,
+    this.isCameraAvailable = false,
+    this.error = "",
+    this.barcode = "",
+  });
 
-  BarcodeScannerStatus(
-      {this.isCameraAvailable = false,
-      this.error = '',
-      this.barcode = '',
-      this.stopScanner = false});
+  factory BarcodeScannerStatus.available() => BarcodeScannerStatus(
+        isCameraAvailable: true,
+        stopScanner: false,
+      );
+  factory BarcodeScannerStatus.error(String message) => BarcodeScannerStatus(
+        error: message,
+        stopScanner: true,
+      );
+  factory BarcodeScannerStatus.barcode(String barcode) => BarcodeScannerStatus(
+        barcode: barcode,
+        stopScanner: true,
+      );
 
-  /*Factory é um construtor nomeado,
-   além do construtor padrão, você pode ter vários tipos para criar sua classe,
-   por isso usamos o factory para definir que queríamos mais um para fazer o fromJson e fromMap
-  */
-  factory BarcodeScannerStatus.available() =>
-      BarcodeScannerStatus(
-          isCameraAvailable: true, stopScanner: false);
-
-  ///quando der erro ele para o scanner
-  factory BarcodeScannerStatus.error(String message) =>
-      BarcodeScannerStatus(error: message, stopScanner: true);
-
-  ///e quando encontrarmos o barcode queremos parar o scanner
-  factory BarcodeScannerStatus.barcode(String barcode) =>
-      BarcodeScannerStatus(barcode: barcode, stopScanner: true);
-
-  ///Só vamos exibir a camera se ela estiver pronta e não tiver nenhum tipo de erro
   bool get showCamera => isCameraAvailable && error.isEmpty;
-
-  ///se tiver algum erro vai verificar ele e tomar uma decição
   bool get hasError => error.isNotEmpty;
-
-  ///só vamos saber se tem o barcode se ele não estiver vazio
   bool get hasBarcode => barcode.isNotEmpty;
+
+  BarcodeScannerStatus copyWith({
+    bool? isAvailable,
+    String? error,
+    String? barcode,
+    bool? stopScanner,
+    CameraController? cameraController,
+  }) {
+    return BarcodeScannerStatus(
+      isCameraAvailable: isAvailable ?? this.isCameraAvailable,
+      error: error ?? this.error,
+      barcode: barcode ?? this.barcode,
+      stopScanner: stopScanner ?? this.stopScanner,
+    );
+  }
 }
